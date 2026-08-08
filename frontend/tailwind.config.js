434bd3556943310f6e9ae01dde61bbcf9d0c4002
Prasanitha Ramana <prasanitharamana@gmail.com>
2026-08-08 18:24:44 +0530

    Update brand colors to match circlestore.in

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,23 @@ export default {
     "./src/**/*.{js,jsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blue: {
+          50: '#f0f6fa',
+          100: '#e0ecf5',
+          200: '#c0d9eb',
+          300: '#92c2df',
+          400: '#5da4ce',
+          500: '#388ac4',
+          600: '#165a9d', // CircleStore Primary Blue
+          700: '#164882',
+          800: '#163e6e',
+          900: '#17365a',
+          950: '#162535', // CircleStore Dark Navy
+        },
+      }
+    },
   },
   plugins: [],
 }
